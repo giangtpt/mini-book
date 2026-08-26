eb9282141cc251123394a24f212bf5f838e6150d
Giang <tran.giang@jvb-corp.com>
2026-08-26 08:28:39 +0700

    Redirect root URL to books list

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('books.index');
 });
 
 Route::resource('categories', CategoryController::class);
